f6a85dba14a0867c4d84cb53b685e3bf9a8010a4
jalal-khan <jalalusafxai@gmail.com>
2023-08-09 16:01:16 +0500

    submit

--- Todo List App/Controllers/HomeController.cs
+++ Todo List App/Controllers/HomeController.cs
@@ -40,7 +40,31 @@ namespace Todo_List_App.Controllers
             model.Tasks = _tasks;
             return View("Index", model);
         }
-
+        public IActionResult Search(string searchString)
+        {
+            ViewData["SearchString"]=searchString;
+            var task = _tasks.ToList();
+            var filteredResult=new List<TaskModel>();
+            var model = new IndexViewModel();
+            if(!string.IsNullOrEmpty(searchString))
+            {
+                filteredResult=task.Where(t=>t.Text.Contains(searchString)).ToList();
+                model.Tasks = filteredResult.ToList();
+            }
+            return View("Index", model);
+            
+        }
+        [HttpPost]
+        public IActionResult UpdateSelectedStatus(string id, bool myCheckbox)
+        {
+            Guid idGuid = Guid.Parse(id);
+            var task = _tasks.FirstOrDefault(x => x.Id == idGuid);
+            if (task != null)
+            { 
+            task.IsSelected = !task.IsSelected;
+            }
+            return RedirectToAction("Index");
+        }
         public IActionResult Add(AddTaskViewModel Request,string taskString)
         {
             ViewData["TaskString"] = taskString;
